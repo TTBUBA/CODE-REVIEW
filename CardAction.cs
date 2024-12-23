163f46d0849ee444044515889b55bbdb0dd87faf
BUBA <122395887+TTBUBA@users.noreply.github.com>
2024-12-23 17:15:10 +0100

    Delete CardAction.cs

--- CardAction.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-//Classe astratta che rappresenta un'azione generica della carta
-public abstract class CardAction : ScriptableObject
-{
-    public abstract void Execute(Card_Info card, CardManager cardManager); // Riceve le informazioni della carta e cardManager della carte come parametri
-}
